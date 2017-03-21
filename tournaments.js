7eac9be2952946d74793e2b7164bd127ecb6570e
Sindre Svendby <sinsvend@gmail.com>
2017-03-21 23:12:41 +0100

    Wip: cheerio does some work

---
+++ tournaments.js
@@ -0,0 +1,84 @@
+({
+    babel: true
+})
+
+import lodash from 'lodash';
+import cheerio from 'cheerio';
+import cheerioTableparser from 'cheerio-tableparser';
+const superagent = require('superagent');
+const a = superagent.agent();
+
+const htmlParseOption = {
+    normalizeWhitespace: false,
+    xmlMode: false,
+    decodeEntities: true
+};
+
+function main() {
+    a.get("https://profixio.com/fx/login.php?login_public=NVBF.NO.SVB")
+    .end(requestTournament)
+}
+
+function requestTournament() {
+    var ids = a.get("https://profixio.com/fx/terminliste.php?vis_gamle_arr=true")
+        .end(getTournmanetIds)
+}    
+    
+const  mapToTournamentUrl = id => `https://profixio.com/fx/vis_innbydelse.php?ib_id=${id}`;
+const parseIdFromUrl = link => link.match(/ib_id=([0-9]*)/)[1];
+
+const ROW_WITH_TOURNAMENT_LINKS = 4;
+
+function getTournmanetIds(err, res) {
+    if(err) {
+        throw err;
+    }
+    const $ = cheerio.load(res.text);
+    cheerioTableparser($)
+    const tableData = $("table").parsetable();
+
+    const tournamentLinksWithHeader = tableData[ROW_WITH_TOURNAMENT_LINKS];
+    const tournamentLinks = tournamentLinksWithHeader.slice(1);
+    const tournamentDetailUrls = tournamentLinks
+        .map(parseIdFromUrl)
+        .map(mapToTournamentUrl)
+    
+    console.log(tournamentDetailUrls)
+    tournamentDetailUrls.forEach(url => {
+        a.get(url).end(handleDetailPage)
+    });
+
+}
+
+function handleDetailPage(err, res) {
+    if(err) {
+        return console.log('error', err);
+    }
+    const $ = cheerio.load(res.text);
+    cheerioTableparser($)
+    const data = $("table").parsetable(true, true, true);
+    const length = data[1].length
+    //tournamentNameIds.push(data[1].length);
+    const tournamentDetails = data[1]
+    getTournamentPage(tournamentDetails[length-1]);
+}
+
+//main()
+
+function getTournamentPage(url) {
+    a.get(url, handleTournamentPage);
+}
+
+function handleTournamentPage(err, res) {
+    if(err) {
+        return console.log('error', err);
+    }
+    console.log(res.text);
+    const $ = cheerio.load(res.text);
+    cheerioTableparser($)
+    
+}
+
+
+
+getTournamentPage("http://www.profixio.com/pamelding/redirect.php?tknavn=rogaland_tour_1_u13_17");
